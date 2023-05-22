b5f14a313e15af82a196d0112dc002594c65a32a
Alberto <109277849+Albertogzl@users.noreply.github.com>
2023-05-22 15:41:04 +0200

    Update streamlit_app.py
    🥋 Now Let's Use a Function and Button to Add the Fruit Name Submissions

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,6 @@ except URLError as e:
 #import requests
 #streamlit.text(fruityvice_response.json()) # just write the data to the screen
 
-
-#import snowflake connector
 #import snowflake.connector
 streamlit.header("The fruit load list contains:")
 #snowflake related functions
@@ -60,14 +58,26 @@ if streamlit.button ('get_fruit_load_list'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
- #don't run anything past here while we troubleshoot
+
+ # Allow user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() s my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from sreamlit')")
+    return 'Thanks for adding '+ new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.dataframe(back_from_function)
+
+
+  #don't run anything past here while we troubleshoot
 streamlit.stop()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") # query metadata
  # query data
 
-# Allow user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+
+streamlit.write()
 
 #this will not work correctly, but we go eith it for now
-my_cur.execute("insert into fruit_load_list values ('from sreamlit')")
