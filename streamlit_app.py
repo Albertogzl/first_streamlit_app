99ba31e393ee63736f50eeabada29d33076bdb95
Alberto <109277849+Albertogzl@users.noreply.github.com>
2023-05-22 15:30:20 +0200

    Update streamlit_app.py
    Moved the Fruit Load List Query and Load into a Button Action

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,14 +50,20 @@ streamlit.stop()
 
 #import snowflake connector
 #import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") # query metadata
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list") # query data
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#snowflake related functions
+def get_fruit_load_list():
+  with my_cur = my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    return my_cur.fetchall()
+# Add a button to load the fruit 
+if streamlit.button ('get_fruit_load_list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") # query metadata
+ # query data
 
 # Allow user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
